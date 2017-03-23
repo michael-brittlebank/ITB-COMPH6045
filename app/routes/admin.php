ac895ed1d7b2c736a72327b95fba65833fa4c266
mike-stumpf <mike.a.stumpf@gmail.com>
2017-03-23 13:04:27 +0000

    admin - updated admin routes

--- app/routes/admin.php
+++ app/routes/admin.php
@@ -1,10 +1,7 @@
 <?php
 
 $app->group('/admin', function () use ($app) {
-    $app->get('', \Controllers\Admin::class.':getDashboardPage');
-
-    $app->group('/products', function () use ($app) {
-        $app->get('/new', \Controllers\Admin::class.':getNewProductPage');
-        $app->get('/edit/{productId}', \Controllers\Admin::class.':getEditProductPage');
-    });
+    $app->get('', \Controllers\Admin::class.':getDashboardPage')->setName('admin');
+    $app->get('/new', \Controllers\Admin::class.':getNewProductPage');
+    $app->get('/edit/{productId}', \Controllers\Admin::class.':getEditProductPage');
 })->add($isUserAdmin);
