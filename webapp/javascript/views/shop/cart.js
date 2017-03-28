3189cead01698f2591b610d01b15967e514e4e3c
mike-stumpf <mike.a.stumpf@gmail.com>
2017-03-28 17:52:51 +0100

    cart - update product quantity working

--- webapp/javascript/views/shop/cart.js
+++ webapp/javascript/views/shop/cart.js
@@ -2,10 +2,10 @@
 
     var $ = jQuery,
         that = app.views.shop.cart,
-        helpers = app.helpers,
         ajax = app.ajax,
         modals = app.modals,
-        removeItemFromCartButtons;
+        removeItemFromCartButtons,
+        updateItemQuantityButtons;
 
     function submitRemoveFromCart(element){
         if (confirm('Are you sure you want to remove this product from your cart?')){
@@ -27,13 +27,46 @@
         }
     }
 
-    this.init = function(){
-        removeItemFromCartButtons = $('.cart-product-button-remove');
+    function submitUpdateQuantity(element){
+        if (confirm('Are you sure you want to update the quantity of this product?')){
+            var id = element.attr('data-product-id'),
+                quantity = $('#cart-product-'+id+'-quantity').val();
+            if (quantity > 0 && quantity < 100) {
+                ajax.ajax(
+                    'PUT',
+                    '/cart',
+                    {
+                        id: id,
+                        quantity: quantity
+                    }
+                )
+                    .then(function () {
+                        window.location.reload();
+                    })
+                    .catch(function () {
+                        modals.openErrorModal('Error', 'Could not update product quantity. Please try again later');
+                    });
+            } else {
+                modals.openErrorModal('Error', 'Please enter a quantity between 1 and 100');
+            }
+        }
+    }
 
-        removeItemFromCartButtons.on('click',function(event){
-            event.preventDefault();
-            submitRemoveFromCart($(this));
-        });
+    this.init = function(){
+        if ($('#page-cart').length > 0) {
+            removeItemFromCartButtons = $('.cart-product-button-remove');
+            updateItemQuantityButtons = $('.cart-product-button-update');
+
+            removeItemFromCartButtons.on('click', function (event) {
+                event.preventDefault();
+                submitRemoveFromCart($(this));
+            });
+
+            updateItemQuantityButtons.on('click', function (event) {
+                event.preventDefault();
+                submitUpdateQuantity($(this));
+            });
+        }
     };
 
 }).apply(app.views.shop.cart);
